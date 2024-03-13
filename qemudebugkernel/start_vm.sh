9ffa0aed35f69eadef08774c6e3b0d72c2823672
hanzhijun <hanzhijun@archeros.com>
2024-03-13 15:17:38 +0800

    update qemudebugkernel

--- qemudebugkernel/start_vm.sh
+++ qemudebugkernel/start_vm.sh
@@ -9,20 +9,16 @@ qemudir=$(jq -r ".qemu_dir" < $configuration)
 kerneldir=$(jq -r ".kernel_dir" < $configuration)
 initrddir=$(jq -r ".initrddir" < $configuration)
 
-
-
-if [[ $1 == '--debug' ]]; then
-cmd="$qemudir/qemu-system-x86_64 \
+basecmd="$qemudir/qemu-system-x86_64 \
      -nographic -m 1024M \
      -kernel $kerneldir/arch/x86/boot/bzImage \
      -hda $initrddir/initrd.img \
-     -append 'noapic root=/dev/sda rw console=ttyS0 earlycon=ttyS0 earlyprint ignore_loglevel' -S -s"
-else
-cmd="$qemudir/qemu-system-x86_64 \
-     -nographic -m 1024M \
-     -kernel $kerneldir/arch/x86/boot/bzImage \
-     -hda $initddir/initrd.img \
      -append 'noapic root=/dev/sda rw console=ttyS0 earlycon=ttyS0 earlyprint ignore_loglevel'"
+
+if [[ $1 == '--debug' ]]; then
+	cmd="$basecmd -S -s"
+else
+	cmd="$basecmd"
 fi
 
 eval $cmd
