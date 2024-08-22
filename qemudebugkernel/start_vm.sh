2bfa8deeb61d4e0570b090946da02556485ff926
junhan <hanzj.it@gmail.com>
2024-08-22 10:43:29 +0800

    modify qemudebugkernel shell

--- qemudebugkernel/start_vm.sh
+++ qemudebugkernel/start_vm.sh
@@ -1,34 +1,69 @@
 #!/bin/bash 
-# configure
 
 pwdpath=$(dirname "$(realpath "$0")")
 
 configuration=${pwdpath}/config.json
 
-qemudir=$(jq -r ".qemu_dir" < $configuration)
-kerneldir=$(jq -r ".kernel_dir" < $configuration)
-initrddir=$(jq -r ".initrddir" < $configuration)
-qcowdir=$(jq -r ".qcowdir" < $configuration)
+qemupath=$(jq -r ".qemupath" < $configuration)
+kernelpath=$(jq -r ".kernelpath" < $configuration)
+initrdpath=$(jq -r ".initrdpath" < $configuration)
+qcowpath=$(jq -r ".qcowpath" < $configuration)
+
+
+echo $qemupath
+echo $kernelpath
+echo $initrdpath
+echo $qcowpath
+checkqemu() {
+
+	if [[ ! -f "$qemupath" ]];then
+		echo "qemu is not"
+	fi
+}
+
+checkkernel() {
+	if [[ ! -f "$kernelpath" ]];then
+		echo "kernel is not"
+	fi
+}
+
+checkinitrd() {
+	if [[ ! -f "$initrdpath" ]];then
+		echo "initrd is not"
+	fi
+}
+
+checkqcow() {
+	if [[ ! -f "$qcowpath" ]];then
+		echo "qcow is not"
+	fi
+}
+
 
 debug_kernel() {
-	cmd="$qemudir/qemu-system-x86_64 \
+	cmd="$qemupath \
 		-nographic -m 1024M \
-		-kernel $kerneldir/arch/x86/boot/bzImage \
-		-hda $initrddir/initrd.img \
-		-append 'noapic root=/dev/sda rw console=ttyS0 earlycon=ttyS0 earlyprint ignore_loglevel'"
+		-kernel $kernelpath \
+		-hda $initrdpath\
+		-append 'noapic root=/dev/sda rw console=ttyS0 earlycon=ttyS0 earlyprint ignore_loglevel' -S -s "
 
 	eval $cmd
 }
 
 start_vm() {
-	cmd="$qemudir/qemu-system-x86_64 -name hanzj-test \
+	cmd="$qemupath -name hanzj-test \
 		 -machine pc -m size=16g -smp 8  \
-		 -drive file=$qcowdir/openEuler-22.03-LTS-SP3-x86_64.qcow2,format=qcow2,if=none,id=disk0 \
+		 -drive file=$qcowpath,format=qcow2,if=none,id=disk0 \
 		 -device virtio-blk-pci,drive=disk0 \
 		 -vnc :2000 -device cirrus-vga,id=video0 -monitor stdio"
 
 	eval $cmd
 }
+
+checkqemu
+checkkernel
+checkqcow
+checkinitrd
 
 if [[ $1 == '--debug_kernel' ]]; then
 	debug_kernel
