c2e3d7c7a32baf58088c0bcc2a4e97832f1534a6
hanzhijun <hanzhijun@archeros.com>
2024-11-01 17:43:13 +0800

    fix mouse use

--- nvim/lua/plugins/option.lua
+++ nvim/lua/plugins/option.lua
@@ -1,3 +1,3 @@
 vim.opt.clipboard = 'unnamedplus' -- use system clipboard
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
+vim.opt.mouse = 'v' -- allow the mouse to be used in Nvim
