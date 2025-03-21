abdef7dc7bc39e93a25a485b739959e75e6334fd
hanzhijun <hanzhijun@archeros.com>
2025-03-21 14:18:42 +0800

    update buildkernelandqemu.sh

--- qemudebugkernel/buildkernelandqemu.sh
+++ qemudebugkernel/buildkernelandqemu.sh
@@ -1,50 +1,58 @@
 #!/bin/bash 
 
-pwdpath=$(dirname "$(realpath "$0")")
-kernelpath="/nvme-volume/hanzj/workenvconfig/qemudebugkernel/bzImage"
-qemupath="/usr/bin/qemu-system-x86_64"
+PWD_PATH=$(dirname "$(realpath "$0")")
+KERNEL_PATH="NULL"
+QEMU_PATH="NULL"
 JSON_FILE="config.json"
 
-#chose 6.10.6 version
+#chose ${KERNEL_VERSION} version
+KERNEL_VERSION="6.10.6"
+QEMU_VERSION="8.2.9"
+KERNEL_FILE="linux-${KERNEL_VERSION}"
+KERNEL_FILEGZ="${KERNEL_FILE}.tar.gz"
+QEMU_FILE="qemu-${QEMU_VERSION}"
+QEMU_FILEXZ=${QEMU_FILE}.tar.xz
 
 buildkernel() {
 	if [ -f '/etc/fedora-release' ];then
-		sudo dnf install gcc make flex bison openssl-devel elfutils-libelf-devel ncurses-devel jq
+		sudo dnf install gcc make flex bison \
+			openssl-devel elfutils-libelf-devel ncurses-devel jq
 	elif [ -f "/etc/arch-release" ] ;then
-		sudo pacman -Sy base-devel bc coreutils cpio gettext initramfs kmod libelf ncurses pahole perl python rsync tar xz
+		sudo pacman -Sy base-devel bc coreutils \
+			cpio gettext initramfs kmod libelf ncurses pahole perl python rsync tar xz
 	fi
 
-	if [ ! -f 'linux-6.10.6.tar.gz' ];then
-		wget --user-agent="Mozilla" https://mirrors.tuna.tsinghua.edu.cn/kernel/v6.x/linux-6.10.6.tar.gz
+	if [ ! -f "$KERNEL_FILEGZ" ];then
+		wget --user-agent="Mozilla" https://mirrors.tuna.tsinghua.edu.cn/kernel/v6.x/linux-${KERNEL_VERSION}.tar.gz
 	fi
 
-	if [ ! -d 'linux-6.10.6' ];then
-		tar -xvf linux-6.10.6.tar.gz
-		cd linux-6.10.6 && make x86_64_defconfig && make -j$(nproc)
-		kernelpath=$(pwd)/arch/x86/boot/bzImage
-		cd $pwdpath
+	if [ ! -d "$KERNEL_FILE" ];then
+		tar -xf linux-${KERNEL_VERSION}.tar.gz
+		cd linux-${KERNEL_VERSION} && make x86_64_defconfig && make -j$(nproc)
+		KERNEL_PATH=$(pwd)/arch/x86/boot/bzImage
+		cd $PWD_PATH
 	else
-	     kernelpath=$(pwd)/linux-6.10.6/arch/x86/boot/bzImage
+		KERNEL_PATH=$(pwd)/linux-${KERNEL_VERSION}/arch/x86/boot/bzImage
 	fi
 }
 
 
 buildqemu() {
-	if [ ! -f 'qemu-6.2.0.tar.xz' ];then
-		wget https://download.qemu.org/qemu-6.2.0.tar.xz
+	if [ ! -f "$QEMU_FILEXZ" ];then
+		wget https://download.qemu.org/qemu-${QEMU_VERSION}.tar.xz
 	fi
 
-	if [ ! -d 'qemu-6.2.0' ];then
-		tar -xvf qemu-6.2.0.tar.xz
-		cd qemu-6.2.0 && mkdir build && \
-		cd build && ../configure --target-list=x86_64-softmmu \
-		--enable-kvm --enable-debug --enable-debug-info --enable-modules \
-		--enable-vnc --enable-trace-backends=log --disable-werror --disable-strip
+	if [ ! -d "$QEMU_FILE" ];then
+		tar -xf qemu-${QEMU_VERSION}.tar.xz
+		cd qemu-${QEMU_VERSION} && mkdir build && \
+			cd build && ../configure --target-list=x86_64-softmmu \
+			--enable-kvm --enable-debug --enable-debug-info --enable-modules \
+			--enable-vnc --enable-trace-backends=log --disable-werror --disable-strip
 		make -j$(nproc)
-		qemupath=$(pwd)/qemu-system-x86_64
-		cd $pwdpath
+		QEMU_PATH=$(pwd)/qemu-system-x86_64
+		cd $PWD_PATH
 	else
-		qemupath=$(pwd)/qemu-6.2.0/build/qemu-system-x86_64
+		QEMU_PATH=$(pwd)/qemu-${QEMU_VERSION}/build/qemu-system-x86_64
 	fi
 }
 
@@ -55,14 +63,27 @@ genConfig() {
 	fi
 
 	if [ -f "$JSON_FILE" ]; then
-     		jq --arg p1 "$kernelpath" --arg p2 "$qemupath" '. + {kernelpath: $p1, qemupath: $p2}' "$JSON_FILE" > temp.json
-			mv temp.json "$JSON_FILE"
+		jq --arg p1 "$KERNEL_PATH" --arg p2 "$QEMU_PATH" \
+			'. + {kernel_path: $p1, qemu_path: $p2}' "$JSON_FILE" > temp.json
+		mv temp.json "$JSON_FILE"
 	else
-		jq -n --arg p1 "$kernelpath" --arg p2 "$qemupath" '{kernelpath: $p1, qemupath: $p2}' > "$JSON_FILE"
+		jq -n --arg p1 "$KERNEL_PATH" --arg p2 "$QEMU_PATH" \
+			'{kernel_path: $p1, qemu_path: $p2}' > "$JSON_FILE"
 	fi
 
 	echo "参数已成功写入 $JSON_FILE"
 
+}
+
+clean() {
+ cd $PWD_PATH
+ rm -rf $KERNEL_FILEGZ
+ rm -rf $KERNEL_FILE
+}
+
+help() {
+
+ echo "help"
 }
 
 
