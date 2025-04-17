42ce33a092ca27ad1904db0aeeecf1b3cfd44eb2
hanzhijun <hanzhijun@archeros.com>
2025-04-17 16:36:15 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -5,39 +5,42 @@ yum install -y ripgrep bear tmux clang git
 yum install -y make gcc flex bison elfutils-devel openssl-devel 
 
 function install_vim() {
+	wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz 
 
-	wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz 
+	rm -rf /opt/nvim-linux-*
+	tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-	rm -rf /opt/nvim*
-	tar -C /opt -xzf nvim-linux64.tar.gz
+	if [ $? -eq 1 ];then
+		sed -i '/nvim-linux-x86_64/d' ~/.bashrc 
+		echo "export PATH="$PATH:/opt/nvim-linux-x86_64/bin"" >> ~/.bashrc 
+		source ~/.bashrc
+	fi
 
-	rm -rf nvim-linux64.tar.gz 
-	sed -i '/nvim-linux64/d' ~/.bashrc 
-	echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc 
-	source ~/.bashrc
-
-	mkdir  ~/.config
-	cp -r  workenvconfig/nvim ~/.config
-	rm -rf workenvconfig
+	mkdir ~/.config
+	cp -r  nvim ~/.config
+	rm -rf nvim-linux-x86_64.tar.gz 
 }
 
 function install_tmux() {
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
 	cp -r workenvconfig/tmux-hanzj/tmux.conf  ~/.tmux.conf
-	echo "alias tmux='tmux -2'" >> ~/.bashrc  #解决tmux 和 vim 颜色不一致
+	grep -q "tmux" ~/.bashrc
+	if [ $? -eq 1 ] ;then
+		echo "alias tmux='tmux -2'" >> ~/.bashrc  #解决tmux 和 vim 颜色不一致
+		echo -e "if [ -z "\$TMUX" ]; then\n\ttmux attach-session || tmux new-session -n $HOSTNAME\nfi" >> ~/.bashrc
+	fi 
 	# 登录服务器自动进入tmux
-	echo -e "if [ -z "\$TMUX" ]; then\n\ttmux attach-session || tmux new-session -n $HOSTNAME\nfi" >> ~/.bashrc
 	source ~/.bashrc
 
 }
 
 function install_git() {
-	cp -r workenvconfig/gitconfig ~/.gitconfig
+	cp -r gitconfig ~/.gitconfig
 }
 
 install_nvim
 install_tmux
-install_git
+#install_git
 
 
 
